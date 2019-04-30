6f5a2ecbdd6e37a5beeb89833de915c46970abc4
liubinbin <liubinbin@hotmail.com>
2019-04-30 17:32:22 +0800

    convert wwn

--- 0410.py
+++ 0410.py
@@ -1,4 +1,19 @@
-print('test')
+#this script convert the str for the sanswitch
 
-#modify
+def wwnformat(wwn):
+    i = 0
+    form = ''
+    wwn = wwn.lower()
+    while i < 16:
+        s = ''.join(wwn[i:i + 2])
+        i = i + 2
+        form = form + ':' + s
+    return form[1:]
 
+finput = open("input.txt","r",encoding="utf8")
+foutput = open("output.txt","w",encoding="utf8")
+for line in finput.readlines():
+    temp=line.lower()
+    foutput.writelines(wwnformat(temp)+"\n")
+finput.close()
+foutput.close()
